6c9a7bb713f72b77519f05c94d358c6f3d491c00
Jared Gilbert <jaredandersgilbert@gmail.com>
2016-11-16 15:58:43 -0500

    refactors, cleans and comments tasks script

--- app/assets/javascripts/_tasks.js
+++ app/assets/javascripts/_tasks.js
@@ -1,38 +1,39 @@
 $(function() {
-  //get all tasks and generate list
-  $.get('/tasks').success(function(data){
-    for (task in data) {
-      $('.todo-list').append(
-        $('<li>').append(
-          $('<input>').attr({
-            class: 'toggle',
-            type: 'checkbox',
-            'data-id': data[task].id,
-            checked: data[task].done
-          }).add($('<label>').append(data[task].title))
-        )
+  //generate task with attributes
+  function generateTask (task){
+    $('.todo-list').append(
+      $('<li>').append(
+        $('<input>').attr({
+          class: 'toggle',
+          type: 'checkbox',
+          'data-id': task.id,
+          checked: task.done
+        }).add($('<label>').append(task.title))
       )
-    };
-
-    //update done status in db on checkbox toggle
-    $('.toggle').change(function (){
-      var task = $(this);
-      var id = task.data("id");
-      var doneStatus = task.prop('checked');
-      $.ajax({
-        url: '/tasks/' + id,
-        method: 'PUT',
-        data: {
-          task: {
-            done: doneStatus
-          }
-        },
-        success: function(){
-          var status = doneStatus ? "done" : "not done";
-          console.log('marked task '+id+' as '+ status);
+    )
+  };
+  //update done status in db on checkbox toggle
+  function updateDoneStatus (){
+    var task = $(this);
+    var id = task.data("id");
+    var doneStatus = task.prop('checked');
+    $.ajax({
+      url: '/tasks/' + id,
+      method: 'PUT',
+      data: {
+        task: {
+          done: doneStatus
         }
-      })
-    });
+      }
+    })
+  };
+  //get all tasks
+  $.get('/tasks').success(function(data){
+    //generate list
+    for (task in data) {
+      generateTask(data[task])
+    };
+    //enable status update
+    $('.toggle').change(updateDoneStatus);
   });
-
 });
