f505876cc5a0838769f2da4e35bcee986450fd17
Jared Gilbert <jaredandersgilbert@gmail.com>
2016-11-17 10:39:38 -0500

    adds create task method and test

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -11,14 +11,24 @@ RSpec.describe TasksController, type: :controller do
 
       response_value = ActiveSupport::JSON.decode(@response.body)
       expect(response_value.count).to eq(2)
-      
+
       response_ids = response_value.collect {|task| task["id"]}
       expect(response_ids).to eq([task1.id,task2.id])
     end
   end
 
+  describe "tasks#create action" do
+    it "should successfully create a task in the db" do
+      post :create, task: {title: 'foo'}
+      expect(response).to have_http_status :success
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value['title']).to eq('foo')
+      expect(Task.last.title).to eq('foo')
+    end
+  end
+
   describe "tasks#update action" do
-    it "should successfuly update tasks in the db" do
+    it "should successfully update tasks in the db" do
       task = FactoryGirl.create(:task)
       put :update, id: task.id, task: {done: true}
       expect(response).to have_http_status :success
